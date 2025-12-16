4253224c4f556a9c1d8449cf6e81317bc803fb93
Di Zhu <judyouo@amazon.com>
2025-12-15 17:28:49 -0800

    Initial Commit

---
+++ MusicStartsHere/MusicStartsHereApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicStartsHereApp.swift
+//  MusicStartsHere
+//
+//  Created by Zhu, Di on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicStartsHereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
